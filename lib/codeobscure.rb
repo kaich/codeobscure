538e06dfdad2f4ea61ff6509f703438ae7eeec9f
kaich <chengkai1853@163.com>
2016-07-26 11:27:21 +0800

    copy shell

--- lib/codeobscure.rb
+++ lib/codeobscure.rb
@@ -2,18 +2,25 @@ require "codeobscure/version"
 require "codeobscure/funclist"
 require "colorize"
 require 'xcodeproj'
+require 'fileutils'
 
 module Codeobscure
+
+  def self.root
+    File.dirname __dir__
+  end
 
   def self.obscure 
     xpj_path = ARGV[0]
     if File.exist? xpj_path
       root_dir = xpj_path.split("/")[0...-1].join "/"
+      shell_path = "#{root}/lib/codeobscure/obscure.sh" 
       FuncList.genFuncList root_dir
       project = Xcodeproj::Project.open xpj_path
       first_target = project.targets.first
       shell_phase = first_target.new_shell_script_build_phase 
       shell_phase.shell_script = "$PROJECT_DIR/obscure.sh"
+      FileUtils.cp shell_path , "#{root_dir}/obscure.sh"
       project.save
     else 
       puts "指定的目录不存在:#{path}".colorize(:red)
